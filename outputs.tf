653cc9250f7a3d1c9d07052760c2e9ba9eb7eb6e
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-19 21:16:25 +0530

    recheck the module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "azurerm_log_analytics_workspace" {
-  value = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  value     = azurerm_log_analytics_workspace.log_analytics_workspace.id
   sensitive = true
 }
